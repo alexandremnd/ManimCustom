0205c8d07cbe37eeff22ec76f705a9344f95ae05
Alexandre <alexxandre.mnd+dev@gmail.com>
2022-04-19 19:01:27 +0200

    Atome

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ManimR import *
 from object import *
 
-config.background_color = "#1d1d1d"
+config.background_color = "#212121"
 
 class Introduction(BetterScene):
     def construct(self):
@@ -27,6 +27,30 @@ class Introduction(BetterScene):
         self.wait(3)
         self.play(Write(title))
         self.wait(2)
+
+
+class Elongation(Scene):
+    def construct(self):
+        carbone = Atom(0.4, GRAY, LEFT + UP)
+        o1 = Atom(0.4, RED, RIGHT + UP)
+        o2 = Atom(0.4, RED, LEFT + DOWN)
+        o3 = Atom(0.4, RED, 2 * LEFT + 2 * UP)
+        pb = Atom(0.6, BLUE, RIGHT + DOWN)
+
+        liaison_c_o1 = Line(carbone.get_center(), o1.get_center())
+        liaison_c_o2 = Line(carbone.get_center(), o2.get_center())
+        liaison_o1_pb = Line(o1.get_center(), pb.get_center())
+        liaison_o2_pb = Line(o2.get_center(), pb.get_center())
+        liaison_c_o3_1 = Line(carbone.get_center() + UR * 0.05, o3.get_center() + UR * 0.05)
+        liaison_c_o3_2 = Line(carbone.get_center() + DL * 0.05, o3.get_center() + DL * 0.05)
+
+        self.add(liaison_c_o3_1, liaison_c_o3_2, liaison_o2_pb, liaison_o1_pb, liaison_c_o2, liaison_c_o1)
+        self.add(carbone, o1, o2, o3, pb)
+        self.wait(1)
+        self.play(Uncreate(liaison_c_o3_1), Uncreate(liaison_c_o3_2), Uncreate(liaison_c_o2), Uncreate(liaison_c_o1), FadeOut(carbone, o3))
+        self.wait(1)
+
+
 
 
 class WavelengthIllustration(Scene):
